ca9ebc08525139d94fd470a1d4ccb631a9974f6f
PabloPassarini <pablo4passarini@gmail.com>
2021-10-27 15:58:48 -0300

    aprendizado_teste

--- Códigos/teste2.py
+++ Códigos/teste2.py
@@ -1,6 +1,30 @@
-def seila():
-    return 1,2,3
+from sklearn import tree
+import sklearn
 
-x,y,z = seila()
 
-print("x->{}  ||  y->{}  ||  z->{}".format(x,y,z))
+lisa = 1
+irregular = 0
+maca = 1
+laranja = 0
+
+pomar = [[150, lisa], [130, lisa], [180, irregular], [160, irregular]]
+
+resultado = [maca, maca, laranja, laranja]
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(pomar, resultado)
+
+peso = input("Entre com o preço: ")
+superficie = input("Entre com a superficie: ")
+
+if superficie == 'lisa':
+    superficie = 1
+else:
+    superficie = 0
+
+resultadoUsuario = clf.predict([[peso, superficie]])
+
+if resultadoUsuario == 1:
+    print("É uma maça")
+else:
+    print("É uma laranja")
